76f70dae68d4f31b511e5e1961143404cf4c6d01
Brian McPhillips <B.McPhillips89@gmail.com>
2020-09-15 01:28:43 -0700

    scrape working with no errors

---
+++ scrape.js
@@ -0,0 +1,8 @@
+const request = require('./lib/request.js');
+const parse = require('./lib/parse.js');
+const store = require('./lib/store.js');
+
+request()
+  .then(document => parse(document))
+  .then(books => store(books))
+  .then(books => console.log(books.length));
